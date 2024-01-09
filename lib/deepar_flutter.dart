d18e261a83d5fb432ec1a3ba1aa660d4c100bf1e
Maximilian Fossler <maximilian.fossler@picture-framing.de>
2024-01-09 17:32:07 +0100

    feat: Export the CameraDirection-enum

--- lib/deepar_flutter.dart
+++ lib/deepar_flutter.dart
@@ -3,3 +3,5 @@ library deepar_flutter;
 export 'src/deep_ar_controller.dart';
 export 'src/deep_ar_preview.dart';
 export 'src/resolution_preset.dart';
+
+export 'src/utils.dart' show CameraDirection;
